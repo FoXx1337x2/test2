116eb60d5a37e52ddedbc40f786ce14808c59087
David Baari <foxxbusiness@hotmail.com>
2017-12-16 19:07:28 +0100

    tu su este vsetky subory zatial teda asi

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	int i,n;
+	char *p_n;
+
+
+	scanf("%d",&n);
+	p_n=(char *)malloc(n*sizeof(char));
+
+	for(i=-1;i<n;i++){
+		scanf("%c",p_n+i);
+	}
+
+	for(i=n-1;i>=0;i--){
+		printf("%c",p_n[i]);
+	}
+
+	return 0;
+}
